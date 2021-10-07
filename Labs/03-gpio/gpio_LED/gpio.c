d620e9b55def6ce379d69fd60783f0ed569bd32a
xczmel00 <xczmel00@vut.cz>
2021-10-07 12:41:49 +0200

    Lab3

--- Labs/03-gpio/gpio_LED/gpio.c
+++ Labs/03-gpio/gpio_LED/gpio.c
@@ -27,7 +27,9 @@ void GPIO_config_output(volatile uint8_t *reg_name, uint8_t pin_num)
 
 void GPIO_config_input_nopull(volatile uint8_t *reg_name, uint8_t pin_num)
 {
-    
+     *reg_name = *reg_name & ~(1<<pin_num);  // Data Direction Register
+     reg_name++;                     // Change pointer to Data Register
+     *reg_name = *reg_name & ~(1<<pin_num);   // Data Register
 }
 
 /**********************************************************************
@@ -54,21 +56,22 @@ void GPIO_config_input_pullup(volatile uint8_t *reg_name, uint8_t pin_num)
 
 void GPIO_write_low(volatile uint8_t *reg_name, uint8_t pin_num)
 {
-    
+    *reg_name = *reg_name & ~(1<<pin_num); 
 }
 
 void GPIO_write_high(volatile uint8_t *reg_name, uint8_t pin_num)
 {
-    
+    *reg_name = *reg_name | (1<<pin_num); 
 }
 
 void GPIO_toggle(volatile uint8_t *reg_name, uint8_t pin_num)
 {
-    
+     *reg_name = *reg_name ^ ~(1<<pin_num); 
 }
 
 
-void GPIO_read(volatile uint8_t *reg_name, uint8_t pin_num)
+uint8_t GPIO_read(volatile uint8_t *reg_name, uint8_t pin_num)
 {
-   
+   if((*reg_name  & (1<<pin_num)) == 0) return (0);
+   else return(1);
 }
